e063f96bdf000edf9ca0ef5d907d6249a1df7bb2
Rakheros <denis.boiseaux@gmail.com>
2025-07-10 12:57:13 +0200

    Initial commit

---
+++ src/main/java/net/rakheros/enchantedrings/EnchantedRingsClient.java
@@ -0,0 +1,10 @@
+package net.rakheros.enchantedrings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnchantedRingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/rakheros/enchantedrings/EnchantedRingsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rakheros.enchantedrings;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnchantedRingsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
